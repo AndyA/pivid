3713b1780aa6c08970c5efa8037f1530bb8e7151
Andy Armstrong <andy@hexten.net>
2013-05-17 15:02:27 +0000

    HLS encoder.

--- tools/hls.sh
+++ tools/hls.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+base="/usr/share/nginx/www"
+
 set -x
 
-rm -rf live
+rm -rf live live.h264 "$base/live"
 mkdir -p live
-raspivid -w 1280 -h 720 -fps 25 -hf -t 120000 -b 3000000 -o - \
-  | ffmpeg -analyzeduration 5000 -y -f h264 -i - -c copy \
-      -f segment -segment_time 8 -segment_format mpegts \
-      "live/%08d.ts" < /dev/null 
+ln -s "$PWD/live" "$base/live"
+mkfifo live.h264
+raspivid -w 1280 -h 720 -fps 25 -vf -t 86400000 -b 1800000 -o live.h264 &
+
+sleep 2
+ffmpeg -y \
+  -i live.h264 \
+  -f s16le -i /dev/zero -r:a 48000 -ac 2 \
+  -c:v copy \
+  -c:a libfaac -b:a 128k \
+  -map 0:0 -map 1:0 \
+  -f segment \
+  -segment_time 8 \
+  -segment_format mpegts \
+  -segment_list "$base/live.m3u8" \
+  -segment_list_size 720 \
+  -segment_list_flags live \
+  -segment_list_type m3u8 \
+  "live/%08d.ts" < /dev/null 
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
-

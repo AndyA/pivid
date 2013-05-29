462a166c9374b5c0ab72c3099dad9c7b2ff45f3a
Andy Armstrong <andy@hexten.net>
2013-05-29 22:44:46 +0100

    Remove old experiments.

--- tools/stream2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-
-raspivid -w 1280 -h 720 -fps 30 -hf -t 9999999 -b 2000000 -o - \
-  | ffmpeg -analyzeduration 5000 -r 30 -f h264 -i - -c copy -f flv rtmp://igloo.fenkle/live/rune
-
-# vim:ts=2:sw=2:sts=2:et:ft=sh
-
